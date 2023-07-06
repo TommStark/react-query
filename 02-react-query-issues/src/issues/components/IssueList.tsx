6fda25810f04cd7013725e72b8434ad8c5e6cb43
TommStark <posik.tomas@gmail.com>
2023-07-06 12:56:53 -0300

    added exercise number 3

--- 02-react-query-issues/src/issues/components/IssueList.tsx
+++ 02-react-query-issues/src/issues/components/IssueList.tsx
@@ -1,6 +1,12 @@
+import { FC } from 'react';
+import { Issue } from '../interfaces';
 import { IssueItem } from './IssueItem';
 
-export const IssueList = () => {
+interface IssueListProps {
+    issues: Issue[];
+}
+
+export const IssueList: FC<IssueListProps> = ({ issues }) => {
     return (
         <div className="card border-white">
             <div className="card-header bg-dark">
@@ -18,8 +24,11 @@ export const IssueList = () => {
             </div>
             <div className="card-body text-dark">
                 {
-                    [1,2,3].map( issue => (
-                        <IssueItem key={issue} />
+                    issues.map( issue => (
+                        <IssueItem 
+                            key={issue.id} 
+                            issue={issue}
+                        />
                     ))
                 
                 }                
